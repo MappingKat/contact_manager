89853f5f8dd86cfcddad531b0af40f7ba81b365b
Katrina Engelsted <katrina@engelsted.co>
2013-11-25 10:54:15 -0700

    make person spec more succinct"

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
-
 describe Person do
-  it 'is valid' do
-    person = Person.new(first_name: 'Alice', last_name: 'Smith')
-    expect(Person.new).to be_valid
+  let(:person) do
+    Person.new(first_name: 'Alice', last_name: 'Smith')
   end
- it 'is invalid without a first name' do
-  person = Person.new(first_name: 'Bob', last_name: nil)
-  expect(person).not_to be_valid
-end
+
+  it 'is valid' do
+    expect(person).to be_valid
+  end
+  
+  it 'is invalid without a first name' do
+    person.first_name = nil
+    expect(person).to_not be_valid
+  end
+
+  it 'is invalid without a last name' do 
+    person.last_name = nil
+    expect(person).to_not be_valid
+  end
 end
